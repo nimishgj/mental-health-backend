22ffd5c188cb5ad3d71738febad29127ad19ae2d
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-11 14:37:45 +0530

    troubleshooting failing workflow

--- iac/terraform/provider.tf
+++ iac/terraform/provider.tf
@@ -1,8 +1,5 @@
-variable "access_key" {}
-variable "secret_key" {}
-
 provider "aws" {
   region     = "ap-south-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = "${ secrets.AWS_ACCESS_KEY_ID }"
+  secret_key = "${ secrets.AWS_SECRET_ACCESS_KEY }"
 }
