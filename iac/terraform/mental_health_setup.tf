43afdd8209bc816821f16db172b3d7f48592f55d
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-17 23:31:46 +0530

    adds deploy job to the workflow file

--- iac/terraform/mental_health_setup.tf
+++ iac/terraform/mental_health_setup.tf
@@ -8,21 +8,28 @@ resource "aws_vpc" "my_vpc" {
 
 # Create a new subnet within the VPC in ap-south-1
 resource "aws_subnet" "my_subnet" {
-  vpc_id             = aws_vpc.my_vpc.id
-  cidr_block         = "10.0.1.0/24"
-  availability_zone  = "ap-south-1a"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "test"
   }
 }
 
-# Create a new security group allowing inbound traffic on port 3000 in ap-south-1
+# Create a new security group allowing inbound SSH and port 3000 traffic in ap-south-1
 resource "aws_security_group" "my_security_group" {
-  name = "terraform-mental-health"
+  name   = "terraform-mental-health"
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
     Name = "test"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP_ADDRESS/32"]  # Replace YOUR_IP_ADDRESS with your actual IP address
   }
 
   ingress {
@@ -38,6 +45,41 @@ resource "aws_security_group" "my_security_group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "test"
+  }
+}
+
+# Attach the internet gateway to the VPC
+resource "aws_vpc_attachment" "my_igw_attachment" {
+  vpc_id             = aws_vpc.my_vpc.id
+  internet_gateway_id = aws_internet_gateway.my_igw.id
+}
+
+# Create a new route table and route for internet access
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "test"
+  }
+}
+
+# Associate the subnet with the route table
+resource "aws_route_table_association" "my_route_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
 }
 
 # Create a new EC2 instance in ap-south-1
