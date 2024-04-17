04e649899ba91e1d55b099317033238f8e79ef8e
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-17 21:59:41 +0530

    adds build and push docker image job to workflow

--- iac/terraform/mental_health_setup.tf
+++ iac/terraform/mental_health_setup.tf
@@ -1,7 +1,25 @@
+# Create a new VPC in ap-south-1
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "test"
+  }
+}
+
+# Create a new subnet within the VPC in ap-south-1
+resource "aws_subnet" "my_subnet" {
+  vpc_id             = aws_vpc.my_vpc.id
+  cidr_block         = "10.0.1.0/24"
+  availability_zone  = "ap-south-1a"
+  tags = {
+    Name = "test"
+  }
+}
+
 # Create a new security group allowing inbound traffic on port 3000 in ap-south-1
 resource "aws_security_group" "my_security_group" {
-  name        = "terraform-mental-health"
-  description = "Allow inbound traffic on port 3000"
+  name = "terraform-mental-health"
+  vpc_id = aws_vpc.my_vpc.id
 
   tags = {
     Name = "test"
@@ -10,13 +28,6 @@ resource "aws_security_group" "my_security_group" {
   ingress {
     from_port   = 3000
     to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -33,6 +44,7 @@ resource "aws_security_group" "my_security_group" {
 resource "aws_instance" "my_ec2_instance" {
   ami                    = "ami-09298640a92b2d12c"
   instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
   key_name               = "ec2-ssh"
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
   associate_public_ip_address = true
