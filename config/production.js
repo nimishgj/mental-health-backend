b61733278a54e5b393b7c7c2e84783144cd4998d
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-18 16:31:12 +0530

    adds database name for production environment

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_PORT: 3000,
-    mongoUri: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`,
+    mongoUri: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/mental-health-prod-db?authSource=admin`,
     mongoOptions: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
