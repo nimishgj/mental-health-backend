128626a6f18a73e2463c570dbd50b54082b90954
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-19 23:01:06 +0530

    troubleshooting failing workflow: test on run

--- test/services/Habits.Service.test.js
+++ test/services/Habits.Service.test.js
@@ -28,7 +28,7 @@ describe("Habits Service", () => {
 
       const result = await HabitsService.deleteHabit(habitId);
       
-      expect(result).toBeNaN();
+      expect(result).toEqual(deletedHabit);
       expect(Habits.findByIdAndDelete).toHaveBeenCalledWith(habitId);
     });
 
