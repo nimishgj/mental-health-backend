b3bfbfb746bc63410b77d497929c580a474e6162
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-22 16:23:50 +0530

    configure ec2 to use exisitng vpc,security group and add userdata

--- iac/terraform/mental_health_setup.tf
+++ iac/terraform/mental_health_setup.tf
@@ -1,90 +1,26 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "mental-health"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
-  tags = {
-    Name = "mental-health"
-  }
-}
-
-resource "aws_security_group" "my_security_group" {
-  name   = "terraform-mental-health"
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "mental-health"
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "mental-health"
-  }
-}
-
-
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-
-  tags = {
-    Name = "mental-health"
-  }
-}
-
-resource "aws_route_table_association" "my_route_association" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
-}
-
 resource "aws_instance" "my_ec2_instance" {
   ami                    = "ami-09298640a92b2d12c"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.my_subnet.id
+  subnet_id              = "subnet-0106b0e347e5e6227"
   key_name               = "ec2-ssh"
-  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  vpc_security_group_ids = ["sg-0e9471f0e58bb18a9"] 
   associate_public_ip_address = true
+  user_data = <<-EOF
+              #cloud-config
+              package_update: true
+              package_upgrade: true
+              runcmd:
+                - yum install -y amazon-efs-utils
+                - apt-get -y install amazon-efs-utils
+                - yum install -y nfs-utils
+                - apt-get -y install nfs-common
+                - file_system_id_1=fs-02e0de1641eb0481f
+                - efs_mount_point_1=/mnt/efs/fs1
+                - mkdir -p "${efs_mount_point_1}"
+                - test -f "/sbin/mount.efs" && printf "\n${file_system_id_1}:/ ${efs_mount_point_1} efs tls,_netdev\n" >> /etc/fstab || printf "\n${file_system_id_1}.efs.ap-south-1.amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\n" >> /etc/fstab
+                - test -f "/sbin/mount.efs" && grep -ozP 'client-info]\nsource' '/etc/amazon/efs/efs-utils.conf'; if [[ $? == 1 ]]; then printf "\n[client-info]\nsource=liw\n" >> /etc/amazon/efs/efs-utils.conf; fi;
+                - retryCnt=15; waitTime=30; while true; do mount -a -t efs,nfs4 defaults; if [ $? = 0 ] || [ $retryCnt -lt 1 ]; then echo File system mounted successfully; break; fi; echo File system not available, retrying to mount.; ((retryCnt--)); sleep $waitTime; done;
+              EOF
   tags = {
     Name = "mental-health"
   }
