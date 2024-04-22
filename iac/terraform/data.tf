9ff3c6260811d0f95bb617cba87079c26bfdddb6
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-22 18:09:14 +0530

    troubleshooting infrastructure setup fail

--- iac/terraform/data.tf
+++ iac/terraform/data.tf
@@ -1,8 +1,3 @@
 data "template_file" "user_data" {
   template = file("${path.module}/templates/user_data.tpl")
-
-  vars = {
-    file_system_id_1 = var.file_system_id
-    efs_mount_point_1 = var.efs_mount_point
-  }
 }
--- iac/terraform/mental_health_setup.tf
+++ iac/terraform/mental_health_setup.tf
@@ -1,19 +1,9 @@
-variable "file_system_id" {
-  type    = string
-  default = "fs-02e0de1641eb0481f"
-}
-
-variable "efs_mount_point" {
-  type    = string
-  default = "/mnt/efs/fs1"
-}
-
 resource "aws_instance" "my_ec2_instance" {
   ami                    = "ami-09298640a92b2d12c"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-0106b0e347e5e6227"
   key_name               = "ec2-ssh"
-  vpc_security_group_ids = ["sg-0e9471f0e58bb18a9"] 
+  vpc_security_group_ids = ["sg-0e9471f0e58bb18a9", "sg-0f604b6c04a69c1f6"]
   associate_public_ip_address = true
   user_data               = data.template_file.user_data.rendered
   tags = {
