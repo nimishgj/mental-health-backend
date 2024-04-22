476fa1f99136c96bd2a8d86c4352c5a977adb31b
Nimisha G J <nimisha.int24@infraspec.dev>
2024-04-22 23:41:04 +0530

    testing infrastructure setup

--- iac/terraform/mental_health_setup.tf
+++ iac/terraform/mental_health_setup.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my_ec2_instance" {
   instance_type          = local.instance_type
   subnet_id              = local.subnet_id
   key_name               = local.key_name
-  vpc_security_group_ids = [local.customized_security_group,local.nfs_security_group]
+  vpc_security_group_ids = [local.customized_security_group]
   associate_public_ip_address = true
   user_data               = data.template_file.user_data.rendered
   tags = {
