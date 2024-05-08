7b291dd5ff9eaccc12fc3405437f6bff2a578504
nimishgj <nimishgj444@gmail.com>
2024-05-08 13:35:47 +0530

    refactor(middleware validation): validation accepts a schema to validate against

--- middleware/Validation.middleware.js
+++ middleware/Validation.middleware.js
@@ -1,15 +1,15 @@
 const { HTTP_STATUS } = require("../constants/HttpStatus") ;
 const { userSchema } = require('../validations/UserValidation');
 
-exports.validateUser  = async (request, response, next) => {
-  try {
-    console.log("Started execution");
-    await userSchema.validate(request.body, { abortEarly: false });
-    next();
-  } catch (error) {
-    console.log(error);
-    console.log("Error occured");
-    return response.status(HTTP_STATUS.BAD_REQUEST).json({error: error.errors});
-  }
-}
+exports.validate = (schema) => {
+  return (request, response, next) => {
+    schema.validate(request.body, { abortEarly: false })
+      .then(() => {
+        next();
+      })
+      .catch((error) => {
+        response.status(HTTP_STATUS.BAD_REQUEST).json({ error: error.errors });
+      });
+  };
+};
 
