e2630a64998d531b2a31f1142bc9dda3aa7b5979
nimishgj <nimishgj444@gmail.com>
2024-05-14 05:08:45 +0530

    prog(userValidation): adds validation for user routes

--- validations/Users.validation.js
+++ validations/Users.validation.js
@@ -17,3 +17,25 @@ exports.userVerificationSchema = yup.object({
   token: yup.number().required(),
 })
 
+exports.changePasswordRequestSchema = yup.object({
+  username: yup.string().required(),
+})
+
+exports.changePasswordSchema = yup.object({
+  token: yup.number().required(),
+  password: yup.string().min(8).max(12).required(),
+  email:  yup.string().email().required(),
+})
+
+exports.forgotPasswordRequestSchema = yup.object({
+  email: yup.string().email().required(),
+})
+  
+exports.deleteUserSchema = yup.object({
+  email: yup.string().email().required(),
+})
+
+exports.changeNotificationPreferenceSchema = yup.object({
+  userId: yup.string().required(),
+  notificationPreference: yup.boolean().required(),
+})
