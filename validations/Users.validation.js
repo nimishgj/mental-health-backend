da2b9a2ae41f583998fdf92c1fc9d21b89613c98
nimishgj <nimishgj444@gmail.com>
2024-05-14 05:07:16 +0530

    refactor(userValidation): move create user validaiton to usersValidation

--- validations/Users.validation.js
+++ validations/Users.validation.js
@@ -5,3 +5,10 @@ exports.userLoginSchema = yup.object({
   email: yup.string().email().required(),
 })
 
+exports.userSchema = yup.object({
+  name: yup.string().required(),
+  username: yup.string().required(),
+  password: yup.string().min(8).max(12).required(),
+  email: yup.string().email().required()
+})
+
