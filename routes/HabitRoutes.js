058209728de3bc0f890e8ffd16f4e235efee7939
nimishgj <nimishgj444@gmail.com>
2024-05-14 06:45:37 +0530

    feat(habitRoutes): adds habit validation middleware to habit routes

--- routes/HabitRoutes.js
+++ routes/HabitRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { createHabit, editHabit, deleteHabit, getHabitsByUserId } = require('../controllers/Habits.Controller');
-
+const { habitValidation } = require('../validations/Habits.validation');
+const { validate } = require('../middleware/Validation.middleware');
 
 const router = express.Router();
 
-router.route("/").post(createHabit);
+router.route("/").post(validate(habitValidation), createHabit);
 
-router.route("/:id").patch(editHabit); 
+router.route("/:id").patch(validate(habitValidation), editHabit); 
 
 router.route("/:id").delete(deleteHabit);
 router.route("/user/:userId").get(getHabitsByUserId);
